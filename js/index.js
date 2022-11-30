24a14738987bb4d6139f0a1c01003251e9c5e45f
semanurbilada <semanurbilada@hotmail.com>
2022-11-30 15:28:53 +0300

    home (main) page designed & js folder created

--- js/index.js
+++ js/index.js
@@ -16,9 +16,9 @@ function currentTime() {
 
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
-    //s = (s < 10) ? "0" + s : s;
+    s = (s < 10) ? "0" + s : s;
 
-    const time = h + ":" + m + " " + period; 
+    let time = h + ":" + m + ":" + s + " " + period; 
     document.getElementById("currentClock").innerText = time;
     document.getElementById("currentClock").textContent = time;
 
@@ -36,3 +36,16 @@ function londonTime() {
 function dubaiTime() {
     pass
 }
+
+/* 
+const d = new Date();
+const localTime = d.getTime();
+const localOffset = d.getTimezoneOffset() * 60000;
+ 
+const utc = localTime + localOffset;
+const offset = 4; // UTC of Dubai is +04.00
+const dubai = utc + (3600000 * offset);
+ 
+const dubaiTimeNow = new Date(dubai).toLocaleString();
+console.log(dubaiTimeNow);
+*/
