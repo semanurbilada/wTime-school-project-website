30d8a55886e12fd39ed75189de8d81712a56da5e
semanurbilada <semanurbilada@hotmail.com>
2022-12-02 20:21:06 +0300

    working on the design(html-css) & some fixes (js)

--- js/index.js
+++ js/index.js
@@ -29,23 +29,34 @@ currentTime();
 
 
 function londonTime() {
-    pass 
-}
-
-
-function dubaiTime() {
-    pass
-}
-
 /* 
 const d = new Date();
 const localTime = d.getTime();
 const localOffset = d.getTimezoneOffset() * 60000;
  
 const utc = localTime + localOffset;
-const offset = 4; // UTC of Dubai is +04.00
+const offset = 0; //London's UTC : +00.00
+const london = utc + (3600000 * offset);
+ 
+const londonTimeNow = new Date(london).toLocaleString();
+console.log(londonTimeNow);
+*/
+}
+londonTime();
+
+
+function dubaiTime() { 
+/* 
+const d = new Date();
+const localTime = d.getTime();
+const localOffset = d.getTimezoneOffset() * 60000;
+ 
+const utc = localTime + localOffset;
+const offset = 4; //Dubai's UTC : +00.00
 const dubai = utc + (3600000 * offset);
  
 const dubaiTimeNow = new Date(dubai).toLocaleString();
 console.log(dubaiTimeNow);
 */
+}
+dubaiTime();
