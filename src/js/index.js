60e8fc4df0d64a7aa206d09e82dfae07a7b334ec
semanurbilada <semanurbilada@hotmail.com>
2022-12-10 20:59:57 +0300

    clock & clock boxes fixed (html-css-js)

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
-const secondStick = document.querySelector('.second');
-const minuteStick = document.querySelector('.minute');
-const hourStick = document.querySelector('.hour');
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
-
+    
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondStick.style.transform = `rotate(${secondsDegrees}deg)`;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const minutes = now.getMinutes();
-    const minuteDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minuteStick.style.transform = `rotate(${minuteDegrees}deg)`;
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((minutes/60)*30) + 90;
-    hourStick.style.transform = `rotate(${hourDegrees}deg)`;
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
