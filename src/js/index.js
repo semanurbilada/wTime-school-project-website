d808dc900c8b9004b4d8358445217fa0603087e6
semanurbilada <semanurbilada@hotmail.com>
2022-12-24 23:27:51 +0300

    file changes

--- src/js/index.js
+++ src/js/index.js
@@ -96,3 +96,30 @@ function currentTime() {
 //variable declaration for using clearInterval later
 const stopCurrentTime = setInterval(currentTime, 1000);
 currentTime();  
+
+
+
+document.getElementById('list').onclick = function() {showList()};
+document.getElementById('searchbar').onclick = function() {showList()};
+
+function showList() {
+    document.getElementById('list').classList.toggle("show");   
+    search();
+}
+
+function search() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    
+    let x = document.getElementsByClassName('city-country');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+            continue;   
+        }
+        else {
+            x[i].style.display = "list-item";  
+        }  
+    }
+} 
