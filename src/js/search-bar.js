72c2c96ebb6115ab5af4d955eb94f6870ce9d763
semanurbilada <semanurbilada@hotmail.com>
2023-01-07 14:09:08 +0300

    update for comments

--- src/js/search-bar.js
+++ src/js/search-bar.js
@@ -27,3 +27,26 @@ function search() {
         }  
     }
 } 
+
+
+
+/* 
+const searchList = [
+    {city: 'Singapore'},
+    {city: 'Rome'},         {country: 'Italy'},
+    {city: 'Seoul'},        {country: 'Korea'},
+    {city: 'Tokyo'},        {country: 'Japan'},
+    {city: 'Oslo'},         {country: 'Norway'},
+    {city: 'Paris'},        {country: 'France'},
+    {city: 'Moscow'},       {country: 'Russia'},
+    {city: 'Berlin'},       {country: 'Germany'},
+    {city: 'Ankara'},       {country: 'Turkiye'},
+    {city: 'Toronto'},      {country: 'Canada'},
+    {city: 'Istanbul'},     {country: 'Turkiye'},
+    {city: 'London'},       {country: 'United Kingdom'},
+    {city: 'Amsterdam'},    {country: 'Netherlands'},
+    {city: 'New York'},     {country: 'United States'},
+    {city: 'Los Angeles'},  {country: 'United States'},
+    {city: 'Dubai'},        {country: 'United Arab Emirates'}
+]
+*/
